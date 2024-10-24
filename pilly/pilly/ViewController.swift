8ad45ccbf19fbad5bbfa868639cc0f16d57cdf27
Belen Tesfaye <tesfaye@northeastern.edu>
2024-10-24 15:10:48 -0400

    Added an Xcode project so the repository opens on xcode

---
+++ pilly/pilly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pilly
+//
+//  Created by Belen Tesfaye on 10/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
