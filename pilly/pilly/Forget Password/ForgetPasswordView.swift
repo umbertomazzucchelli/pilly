bd2f91b6947e173857b9bcace475c4a749f4c86e
Belen Tesfaye <tesfaye@northeastern.edu>
2024-10-26 01:12:16 -0400

    Complete UI for Forget Password View - Designed layout with app icon, email input, and submit button. - Implemented basic validation for email submission. - Tested UI responsiveness and functionality.

--- pilly/pilly/Forget Password/ForgetPasswordView.swift
+++ pilly/pilly/Forget Password/ForgetPasswordView.swift
@@ -7,14 +7,63 @@
 
 import UIKit
 
+
 class ForgetPasswordView: UIView {
+    
+    var appIconImageView: UIImageView!
+    var emailTextField: UITextField!
+    var submitButton: UIButton!
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        setupAppIconImageView()
+        setupEmailTextField()
+        setupSubmitButton()
+        initConstraints()
     }
-    */
+    
+    func setupSubmitButton() {
+        submitButton = UIButton(type: .system)
+        submitButton.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(submitButton)
+    }
 
+    func setupEmailTextField() {
+        emailTextField = UITextField()
+        emailTextField.placeholder = "Email"
+        emailTextField.borderStyle = .roundedRect
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(emailTextField)
+    }
+
+    func setupAppIconImageView() {
+        appIconImageView = UIImageView()
+        appIconImageView.translatesAutoresizingMaskIntoConstraints = false
+        appIconImageView.image = UIImage(named: "pillyIcon")
+        addSubview(appIconImageView)
+    }
+
+    func initConstraints() {
+        NSLayoutConstraint.activate([
+            appIconImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            appIconImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 40),
+            appIconImageView.widthAnchor.constraint(equalToConstant: 200),
+            appIconImageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            emailTextField.topAnchor.constraint(equalTo: appIconImageView.bottomAnchor, constant: 20),
+            emailTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            emailTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            
+            submitButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            submitButton.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 32)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- pilly/pilly/Forget Password/ForgetPasswordViewController.swift
+++ pilly/pilly/Forget Password/ForgetPasswordViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ForgetPasswordViewController: UIViewController {
+    
+    let forgetPasswordScreen = ForgetPasswordView()
+    
+    override func loadView() {
+        view = forgetPasswordScreen
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = "Forgot Password"
+        
+        forgetPasswordScreen.submitButton.addTarget(self, action: #selector(sendEmail), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
+    
+    @objc func sendEmail() {
+        if let email = forgetPasswordScreen.emailTextField.text, !email.isEmpty {
+            if isValidEmail(email) {
+                showAlert(title: "Email Sent", message: "If an email address is linked to an existing account, we will send instructions on how to reset your password.")
+            } else {
+                showAlert(title: "Invalid Email", message: "Please enter a valid email address.")
+            }
+        } else {
+            showAlert(title: "Input Error", message: "Email field cannot be empty.")
+        }
+    }
+
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+
+    
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailTest.evaluate(with: email)
+    }
+    
+    
+    
+    
+    
     
 
     /*
@@ -27,3 +68,4 @@ class ForgetPasswordViewController: UIViewController {
     */
 
 }
+
