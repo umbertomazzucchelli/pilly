68a37451607f6ac380e44ddc51877a27b7346389
Belen Tesfaye <tesfaye@northeastern.edu>
2024-10-26 01:07:23 -0400

    Complete UI for Forget Password View and test basic functionalities

---
+++ pilly/pilly/ForgetPasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  ForgetPasswordView.swift
+//  pilly
+//
+//  Created by Belen Tesfaye on 10/24/24.
+//
+
+import UIKit
+
+class ForgetPasswordView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
